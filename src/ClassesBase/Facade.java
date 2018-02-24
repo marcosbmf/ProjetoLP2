3b7d99057cb940b882d5d7c63be869c44c592daf
Victor Miranda <victruu@outlook.com>
2018-02-24 20:58:28 -0300

    Adicionando facade

--- src/ClassesBase/Facade.java
+++ src/ClassesBase/Facade.java
@@ -2,39 +2,41 @@ package ClassesBase;
 
 public class Facade {
 	
+	AlunoController alunocontroller = new AlunoController();
+	TutorController tutorcontroller = new TutorController();
 	
 	public void cadastrarAluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
-	
+		alunocontroller.cadastrarAluno(nome,matricula,codigoCurso,telefone,email);
 	}
 	public String recuperaAluno(String matricula) {
-		return "";
+		return alunocontroller.recuperaAluno(matricula);
 	}
 	public String listarAlunos() {
-		return "";
+		return alunocontroller.listarAlunos;
 	}
 	public String getInfoAluno(String matricula, String atributo) {
-		return "";
+		return alunocontroller.getInfoAluno(matricula,atributo);
 	}
 	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
-		
+		tutorcontroller.tornarTutor(matricula,disciplina,proficiencia);
 	}
 	public String recuperaTutor(String matricula) {
-		return "";
+		return tutorcontroller.recuperaTutor(matricula);
 	}
 	public String listarTutores() {
-		return "";
+		return tutorcontroller.listarTutores;
 	}
 	public void cadastrarHorario(String email, String horario, String dia) {
-		
+		tutorcontroller.cadastrarHorario(email,horario,dia);
 	}
 	public void cadastrarLocalDeAtendimento(String email, String local) {
-		
+		tutorcontroller.cadastrarLocalDeAtendimento(email,local);
 	}
 	public boolean consultaHorario(String email, String horario, String dia) {
-		return true;
+		return tutorcontroller.consultarHorario(email,horario,dia);
 	}
 	public boolean consultaLocal(String email, String local) {
-		return false;
+		return tutorcontroller.consultaLocal(email,local);
 	}
 	
 }
