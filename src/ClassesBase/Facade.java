9b263138909958e830cd5e609fed0f3014d9221d
Victor Miranda <victruu@outlook.com>
2018-02-24 20:35:01 -0300

    Adicionando metodos a Facade

--- src/ClassesBase/Facade.java
+++ src/ClassesBase/Facade.java
@@ -4,10 +4,8 @@ public class Facade {
 	
 	
 	public void cadastrarAluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
-		
+	
 	}
-	
-	
 	public String recuperaAluno(String matricula) {
 		return "";
 	}
@@ -16,6 +14,27 @@ public class Facade {
 	}
 	public String getInfoAluno(String matricula, String atributo) {
 		return "";
+	}
+	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
+		
+	}
+	public String recuperaTutor(String matricula) {
+		return "";
+	}
+	public String listarTutores() {
+		return "";
+	}
+	public void cadastrarHorario(String email, String horario, String dia) {
+		
+	}
+	public void cadastrarLocalDeAtendimento(String email, String local) {
+		
+	}
+	public boolean consultaHorario(String email, String horario, String dia) {
+		return true;
+	}
+	public boolean consultaLocal(String email, String local) {
+		return false;
 	}
 	
 }
