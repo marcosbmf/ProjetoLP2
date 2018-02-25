a02f0ffa9d0f49f3caca6fd1646dc23e037ede74
Jonathan Lucas <jonathan.morais@ccc.ufcg.edu.br>
2018-02-25 17:09:42 -0300

    Modificações da Classe tutorControler

--- src/ClassesBase/TutorController.java
+++ src/ClassesBase/TutorController.java
@@ -10,45 +10,64 @@ public class TutorController {
 
 	private Map<String, Tutor> tutores = new HashMap<String, Tutor>();
 
-	public void tornarTutor(Aluno aluno, String disciplina, int proficiencia) {
-		Tutor novoTutor = new Tutor(aluno.getMatricula(), aluno.getNome(), aluno.getCodigoCurso(), aluno.getTelefone(),
-				aluno.geteMail(), aluno.getNotaAvalicao(), disciplina, proficiencia);
-
-		tutores.put(aluno.getMatricula(), novoTutor);
-
+	public Tutor tornarTutor(String nome, String matricula, int codigoCurso, String telefone,
+			String email, double notaAvaliacao, String disciplina, int proficiencia) {
+		
+		Tutor novoTutor = new Tutor(matricula, nome, codigoCurso, telefone, 
+				email, notaAvaliacao, disciplina, proficiencia);
+		
+		tutores.put(matricula, novoTutor);
+		
+		return novoTutor;
 	}
-
+	
 	public String recuperaTutor(String matricula) {
 		return tutores.get(matricula).toString();
 	}
+	
+	public String listarTutores()  {
 
-	public String listarTutores() {
-
-		List<Tutor> tutores = (List<Tutor>) this.tutores.values();
-		String aux = "";
-
-		// Collections.sort((List<T>) tutores);
-		for (Aluno aluno : tutores) {
-			aux += tutores.toString() + System.lineSeparator();
-		}
-
+		List<Tutor> tutores =  (List<Tutor>) this.tutores.values() ;
+		String aux = "" ;
+		Collections.sort(tutores);
+		for(Tutor tutor : tutores) {
+			aux += tutor.toString() + System.lineSeparator() ;
+		}		
 		return aux;
+		
 	}
-
+	
 	public void cadastrarHorario(String email, String horario, String dia) {
-
+		this.tutores.get(procuraTutor(email)).cadastrarHorario(horario, dia);
 	}
-
-	public void cadastrarLocalDeAtendimento(String email, String local) {
-
+	
+	public void cadastrarLocalDeAtendimento(String email, String local)  {
+		this.tutores.get(procuraTutor(email)).cadastrarLocalDeAtendimento(local);
 	}
-
+	
 	public boolean consultaHorario(String email, String horario, String dia) {
-		return true;
+		return this.tutores.get(procuraTutor(email)).consultaHorario(horario, dia);
+	}
+	
+	public boolean consultaLocal(String email, String local)  {
+		return 	this.tutores.get(procuraTutor(email)).consultaLocal(local);
+	}
+	
+	public void pagarTutor(String matricula, int quantidade) {
+		
 	}
 
-	public boolean consultaLocal(String email, String local) {
-		return false;
+	
+	private String procuraTutor(String email) {
+		String matricula = "";
+		for (String key : tutores.keySet()) {
+			if(tutores.get(key).geteMail() == email) {
+				matricula = tutores.get(key).getMatricula();
+			}
+					
+		}
+		return matricula;
 	}
+	
 
 }
