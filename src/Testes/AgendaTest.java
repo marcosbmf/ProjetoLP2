3e68dd90ceada7569a87702bad1655f84c477a98
thyagopereira <thyagops@lcc2-09.lcc.ufcg.edu.br>
2018-02-27 09:22:45 -0300

    Primeira parte Testes de unidade Agenda

--- src/Testes/AgendaTest.java
+++ src/Testes/AgendaTest.java
@@ -2,46 +2,130 @@ package Testes;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import ClassesBase.Agenda;
 import ClassesBase.Facade;
 import ClassesBase.Sistema;
 import ClassesBase.Tutor;
 
 public class AgendaTest {
-
+	
+	Agenda agenda;
+	
+	@Before
+	public void testConstrutor() {
+		agenda = new Agenda();	
+	}
+	
 	@Test
-	public void test() {
-		Tutor  tutor = new  Tutor("nome", "matricula", 10 , "123123", "pica@gmai.com", 2 , "aaa", 2);
-		tutor.cadastrarHorario("15:30", "seg");
-		tutor.cadastrarHorario("15:00", "ter");
-
-		assertEquals(true,tutor.consultaHorario("15:30", "seg"));
-		assertEquals(true,tutor.consultaHorario("15:00", "ter"));	
+	public void testCadastraHorario() {
+		agenda.cadastrarHorario("15:00","ter");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastraHorarioVazio() {
+		agenda.cadastrarHorario("","ter");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastraHorarioBranco() {
+		agenda.cadastrarHorario("   ", "dia");
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastraHorarioNull() {
+		agenda.cadastrarHorario(null,"dia");
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastraHoraioDiaVazio() {
+		agenda.cadastrarHorario("15:00", "");
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastrahorarioDiaBranco() {
+		agenda.cadastrarHorario("15:00","  ");
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCadastraDiaNull() {
+		agenda.cadastrarHorario("15:00", null);
+	}
+	
+	
+	@Test
+	public void testConsultaHorarioTrue() {
+		agenda.cadastrarHorario("15:00", "seg");
+		assertEquals(true,agenda.consultaHorario("15:00", "seg"));
 		
 	}
 	
 	@Test
-	public void testconroller() {
-		Sistema sis = new Sistema();
-		sis.cadastrarAluno("nome", "matricula", 10 , "123123", "pica@gmai.com");
-		sis.tornarTutor("matricula","engenharia da putaria", 1);
-		sis.cadastrarHorario("pica@gmai.com", "15:00", "seg");
-		assertEquals(true,sis.consultaHorario("pica@gmai.com", "15:00", "seg"));
+	public void testConsultaHorarioFalse() {
+		agenda.cadastrarHorario("15:00", "ter");
+		assertEquals(false,agenda.consultaHorario("15:00", "seg"));
 	}
-	 
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConsultaHorarioHoraVazia() {
+		agenda.cadastrarHorario("15:00","seg");
+		agenda.consultaHorario("", "seg");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConsultaHorarioHoraBranco() {
+		agenda.cadastrarHorario("15:00","ter");
+		agenda.consultaHorario("  ", "ter");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConsultaHorarioDiaVazio() {
+		agenda.cadastrarHorario("15:00","ter");
+		agenda.consultaHorario("15:00", "");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConsultaHorarioDiaBranco() {
+		agenda.cadastrarHorario("15:00","ter");
+		agenda.consultaHorario("15:00", "    ");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testConsultaHorarioDiaNull() {
+		agenda.cadastrarHorario("15:00","ter");
+		agenda.consultaHorario("15:00", null);
+	}
+	
 	@Test
-	public void testfacade() {
-		Facade fac=  new Facade ();
-		fac.cadastrarAluno("nome", "matricula", 10 , "123123", "pica@gmai.com");
-		fac.tornarTutor("matricula","engenharia da putaria", 1);
-		fac.cadastrarHorario("pica@gmai.com", "15:00", "seg");
-		assertEquals(true,fac.consultaHorario("pica@gmai.com", "15:00", "seg"));
+	public void testCadastraLocal() {
+		agenda.cadastrarLocalDeAtendimento("place");
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastraLocalVazio() {
+		agenda.cadastrarLocalDeAtendimento("");
+	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void testCadastraLocalBranco() {
+		agenda.cadastrarLocalDeAtendimento("    ");
+	}
 	
+	@Test(expected = NullPointerException.class)
+	public void testCadastraLocalNull() {
+		agenda.cadastrarLocalDeAtendimento(null);
+	}
 	
+	@Test
+	public void testConsultaLocal() {
+		agenda.cadastrarLocalDeAtendimento("place");
+		assertEquals(true,agenda.consultaLocal("place"));
+	}
 	
 
 }
