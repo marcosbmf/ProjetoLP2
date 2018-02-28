08db5084df0973d704d9a11612c26975b42f5da6
thyagopereira <31775969+thyagopereira@users.noreply.github.com>
2018-02-27 22:26:37 -0300

    Teste de unidade falho TutorController

--- src/Testes/TutorControllerTest.java
+++ src/Testes/TutorControllerTest.java
@@ -4,11 +4,51 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TutorControllerTest {
+import ClassesBase.TutorController;
+import junit.framework.Assert;
 
+public class TutorControllerTest {
+	
+	TutorController tc ;
+	
 	@Test
 	public void testConstrutor() {
-		
+		tc = new TutorController();
 	}
+	
+	public void testTornarTutor() {
+		tc.tornarTutor("clairo", "666", 666, "666", "666@666.com", 4.5, "lofi", 10);
+	}
+	
+	
+	@Test(expected = NullPointerException.class)
+	public void testTornarTutorProfzero() {
+		tc.tornarTutor("clairo", "666", 666, "666", "666@666.com", 4.5, "lofi", 0);
+
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testTornarTutorProfNegativa() {
+		tc.tornarTutor("clairo", "666", 666, "666", "666@666.com", 4.5, "lofi", -10);
+
+	}
+	
+	
+	@Test
+	public void TestCadastraHorario() {
+		tc.tornarTutor("clairo", "666", 666, "666", "666@666.com", 4.5, "lofi", 10);
+		tc.cadastrarHorario("666@666.com", "15:30", "seg");
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
